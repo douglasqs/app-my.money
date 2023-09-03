875ed4ab346d0ca96ed80dbdb38145590793a96a
Nozard <konzentaru@gmail.com>
2023-09-03 19:03:11 -0300

    refatorado os repositorios

--- lib/src/modules/login/repositories/login_repository.dart
+++ lib/src/modules/login/repositories/login_repository.dart
@@ -3,7 +3,7 @@ import 'package:my_money/src/http/app_dio.dart';
 import 'package:my_money/src/modules/login/model/login_model.dart';
 
 class LoginRepository {
-  String baseURL = "http://192.168.31.96:3099/auth/login";
+  String baseURL = "http://187.35.246.220:10000/auth/login";
 
   Future<Response<Map<String, dynamic>>> sendData(LoginModel loginData) async {
     Dio clientHTTP = await AppDio.getConnection(isAuth: false);
